05e651f6779b5a2e7ad7d1852d49dd3fbcaf3354
beckmackenzie <beckmack@iu.edu>
2025-12-19 15:42:59 -0500

    Delete anovafunctions directory

--- anovafunctions/R/run_oneway_app.R
+++
@@ -1,8 +0,0 @@
-#' Run the One-way ANOVA Shiny App
-#' @importFrom shiny runApp
-#' @export
-run_oneway_app <- function() {
-  app_dir <- system.file("shiny", "oneway_app", package = "anovafunctions")
-  if (app_dir == "") stop("Could not find the Shiny app directory. Try reinstalling.")
-  shiny::runApp(app_dir, display.mode = "normal")
-}
